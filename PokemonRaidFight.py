cb9bb93e5f71e29f5231f44ded8f15f15ef5fe85
daMarxrider <lukasmarxreiter.lm@gmail.com>
2020-08-13 04:37:45 +0200

    improvements

--- PokemonRaidFight.py
+++ PokemonRaidFight.py
@@ -18,16 +18,29 @@ def getTextFromCaptureDevice(inst=None, capture_device=None):
     # img = Image.fromarray(frame)
     dimg = ImageEnhance.Sharpness(img).enhance(1)
     cimg = ImageOps.grayscale(dimg)
+    width, height = cimg.size
+
+    left = 0
+    top = height / 2
+    right = 0
+    bottom = 0
     text = pytesseract.image_to_string(dimg)
     print("text")
     print(text)
+    grey_text = pytesseract.image_to_string(cimg)
+    print("grey_text")
+    print(grey_text)
+    cimg = cimg.crop((left, top, right, bottom))
+    grey_text = pytesseract.image_to_string(cimg)
+    print("cropped_grey_text")
+    print(grey_text)
     return text.strip()
 
 
 class PokemonRaidFight(JoycontrolPlugin):
     async def run(self):
         print("connected")
-        await self.wait(10)
+        await self.wait(5)
         await self.button_push('home')
         await self.wait(2)
         while True:
@@ -52,7 +65,7 @@ class PokemonRaidFight(JoycontrolPlugin):
             await self.button_push('a')
             await self.wait(1)
             await self.button_push('a')
-            await self.wait(30)
+            await self.wait(50)
             # fight started
             print("fight started")
             print(getTextFromCaptureDevice())
@@ -66,6 +79,7 @@ class PokemonRaidFight(JoycontrolPlugin):
                         and "catching"
                         not in getTextFromCaptureDevice().lower())
                    or "is weak" in getTextFromCaptureDevice().lower()):
+                    print("dont catch")
                     await self.wait(1)
                     await self.button_push('down')
                     await self.wait(1)
@@ -73,15 +87,17 @@ class PokemonRaidFight(JoycontrolPlugin):
                     await self.wait(1)
                     break
                 elif "catching" in getTextFromCaptureDevice().lower():
+                    print("aborting catch")
                     await self.button_push('b')
                     await self.wait(1)
                     await self.button_push('down')
                     await self.wait(1)
                     await self.button_push('a')
                     await self.wait(1)
+                    break
 
                 await self.button_push('a')
-                await self.wait(2)
+                await self.wait(1)
             # result screen
             print("result screen")
             text = getTextFromCaptureDevice()
