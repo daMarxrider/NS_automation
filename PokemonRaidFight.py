d9c49c7f7744e04fcc7d7a7bf2be41e2da9caa06
daMarxrider <lukasmarxreiter.lm@gmail.com>
2020-08-23 15:39:03 +0200

    fixed readme and performance of raid fights

--- PokemonRaidFight.py
+++ PokemonRaidFight.py
@@ -16,27 +16,59 @@ def getTextFromCaptureDevice(inst=None, capture_device=None):
     if not ret:
         print("error")
         return
+    texts = []
     img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     # img = Image.fromarray(frame)
     dimg = ImageEnhance.Sharpness(img).enhance(1)
-    cimg = ImageOps.grayscale(dimg)
-    width, height = cimg.size
+    gray_img = ImageOps.grayscale(dimg)
+    inverted_img = ImageOps.invert(gray_img)
+    texts.append(pytesseract.image_to_string(inverted_img))
+    width, height = inverted_img.size
+    left = width/100*4
+    top = height / 100*90
+    right = width/100*6
+    bottom = height/100*94
+    overworld_button = inverted_img.crop((left, top, right, bottom))
+    texts.append(pytesseract.image_to_string(
+        overworld_button, config='--psm 10'))
+
+    left = width/2
+    top = height / 2
+    right = width
+    bottom = height
+    raid_start = gray_img.crop((left, top, right, bottom))
+    texts.append(pytesseract.image_to_string(raid_start))
+
+    left = width/4*3
+    top = height / 2
+    right = width
+    bottom = height/8*6
+    in_raid = inverted_img.crop((left, top, right, bottom))
+    texts.append(pytesseract.image_to_string(in_raid))
+
+    left = width/2
+    top = height/2
+    right = width
+    bottom = height
+    catch_img = inverted_img.crop((left, top, right, bottom))
+    texts.append(pytesseract.image_to_string(catch_img))
+
+    left = width/2
+    top = 0
+    right = width
+    bottom = height/2
+    result_img = inverted_img.crop((left, top, right, bottom))
+    texts.append(pytesseract.image_to_string(result_img))
 
     left = 0
     top = height / 2
     right = width
     bottom = height
     text = pytesseract.image_to_string(dimg)
-    print("text")
-    print(text)
-    grey_text = pytesseract.image_to_string(cimg)
-    print("grey_text")
-    print(grey_text)
-    cimg = cimg.crop((left, top, right, bottom))
-    grey_text = pytesseract.image_to_string(cimg)
-    # print("cropped_grey_text")
-    # print(grey_text)
-    return text.strip()
+
+    result_text = '\n'.join(texts)
+    # print(result_text)
+    return result_text
 
 
 def getOverworldYButton(inst=None, capture_device=None):
@@ -58,7 +90,7 @@ def getOverworldYButton(inst=None, capture_device=None):
     bottom = height/100*94
     img = img.crop((left, top, right, bottom))
     text = pytesseract.image_to_string(img, config='--psm 10')
-    print(text)
+    # print(text)
     return text.strip()
 
 
@@ -184,49 +216,51 @@ class PokemonRaidFight(JoycontrolPlugin):
         rounds = 0
         while True:
             while True:
-                text_bubble = getTextFromTextBubbleOverworld()
+                complete_text = getTextFromCaptureDevice()
+                # text_bubble=getTextFromTextBubbleOverworld()
                 if getOverworldYButton() == 'Y':
                     print("starting before den")
                     await self.button_push('a')
                     await self.wait(2)
-                elif text_bubble.__contains__("Want to throw in"):
+                elif complete_text.__contains__("Want to throw in"):
                     await self.wait(1)
                     await self.button_push('a')
-                elif text_bubble.__contains__("like to save"):
+                elif complete_text.__contains__("like to save"):
                     await self.wait(1)
                     await self.button_push('a')
-                elif text_bubble.__contains__("saved"):
+                elif complete_text.__contains__("saved"):
                     await self.wait(2)
                     await self.button_push('a')
-                elif text_bubble.__contains__("into the den"):
+                elif complete_text.__contains__("into the den"):
                     await self.wait(1)
                     await self.button_push('a')
                     await self.wait(1)
                     await self.button_push('a')
-                elif getTextFromRaidStart().__contains__('Don\'t Invite Others'):
+                elif complete_text.__contains__('Don\'t Invite Others'):
                     await self.wait(1)
                     await self.button_push('down')
                     await self.wait(0.3)
                     await self.button_push('a')
                     await self.wait(1)
                     await self.button_push('a')
-                elif getTextFromInRaidMenu().__contains__('Fight') or getTextFromInRaidMenu().__contains__('Cheer On'):
+                elif complete_text.__contains__('Fight') or complete_text.__contains__('Cheer On'):
                     await self.button_push("a")
                     await self.wait(1)
                     await self.button_push("a")
                     await self.wait(1)
                     await self.button_push("a")
-                elif getTextFromCatchPrompt().__contains__("Catch"):
+                elif complete_text.__contains__("Catch"):
                     print("dont catch")
                     await self.wait(1)
                     await self.button_push('down')
                     await self.wait(1)
                     await self.button_push('a')
-                elif getRaidResult().__contains__("You defeated"):
-                    await self.wait(1)
-                    await self.button_push('a')
                     rounds += 1
                     print(f'rounds: {rounds}')
+                    await self.wait(1)
+                elif complete_text.__contains__("You defeated"):
+                    await self.wait(1)
+                    await self.button_push('a')
                     break
                 else:
                     await self.wait(0.2)
--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 for istallation instructions and requirements look at the 2 linked submodules.
 
 # Pokemon SwordShield Automation
-currently only PokemonRaidFight.py
-example on how to start is in starter.sh
-before starting the script, go to a raid den, face the raid den and then head to the switch home menu(Pokemon SwordShield only allows one connected Controller per time, so this is necessary until i find a workaround... you also should connect every usb controller, just in case)
+currently only PokemonRaidFight.py working  
+example on how to start is in starter.sh(replace bluetooth address with the address of your switch)  
+before starting the RaidFight script, go to a raid den, face the raid den and then head to the switch home menu(Pokemon SwordShield only allows one connected Controller at a time, so this is necessary until i find a workaround... you also should disconnect every usb controller, just in case)
